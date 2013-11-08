1f4fc464953e03e3cbe2a131d9d82a37c5a1cdba
geneeves <geneeves@users.noreply.github.com>
2013-11-08 11:08:28 -0800

    Update photos_pages_spec.rb

--- spec/features/photos_pages_spec.rb
+++ spec/features/photos_pages_spec.rb
@@ -5,7 +5,7 @@ feature "Adding a photo" do
   scenario "with valid information" do
     user = create_and_sign_in_user
     album = FactoryGirl.create(:album, { user: user })
-    visit "/albums/#{album.id}"
+    visit album_path(album)
     click_on "Add a photo"
     fill_in "Image Name", :with => 'cute kitten'
     attach_file "Upload File", "spec/kitten.jpg"
