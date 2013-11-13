114b8bb4c3632c631466abd26eeb14f3e2013b28
Geneva <geneva.skeen@gmail.com>
2013-11-13 12:41:20 -0800

    add s3 host name

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,3 @@
 Paperclip::Attachment.default_options[:url] = 'http://s3.amazonaws.com/pictr/photos'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
