31506e1fb6c671f5f4a92bc2ebef91e4753df8e9
Geneva <geneva.skeen@gmail.com>
2013-11-13 12:34:48 -0800

    alter s3 config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Pictr::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "http://s3.amazonaws.com/pictr/photos"
+  # config.action_controller.asset_host = "http://s3.amazonaws.com/pictr/photos"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,2 @@
+Paperclip::Attachment.default_options[:url] = 'http://s3.amazonaws.com/pictr/photos'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
